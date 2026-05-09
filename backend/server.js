0f6f5644311ead24c097d5436cf0da635b8aba8a
Matheus <matheus.victor.ss48@gmail.com>
2026-05-09 11:54:55 -0300

    adicionando anotações para alterações futuras

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,62 @@
-const express = require("express");//recebe json
-const cors = require("cors");//permite conversar com front
-const db = require("./database");
+//importando as dependências necessárias
+const express = require("express");
+const cors = require("cors");
+const db = require("./database");//importa o banco do database.js
+//biblioteca que cria o servidor
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-app.post("/produtos", (req, res) => {//recebe os dados do script.js
+app.use(cors());//permite front conversar com back
+app.use(express.json());//permite receber JSON
 
+
+// LISTAR PRODUTOS
+//responde a requisição GET para /produtos
+app.get("/produtos", (req, res) => {
+
+    db.all("SELECT * FROM produtos", [], (err, rows) => {//busca no banco
+
+        if (err) {
+            return res.status(500).json({
+                erro: err.message
+            });
+        }
+        //retorna os produtos encontrados em formato JSON
+        res.json(rows);
+    });
+});
+
+
+// CADASTRAR PRODUTO
+app.post("/produtos", (req, res) => {
+    //pega os dados
+    const {codigo_barras, nome, descricao, quantidade, preco } = req.body;
+
+    db.run(//salvando no banco
+        `
+        INSERT INTO produtos
+        (codigo_barras, nome, descricao, quantidade, preco)
+        VALUES (?, ?, ?, ?, ?)
+        `,//???? são os placeholders, os espaços la no banco
+        //e sao substituidos pelos valores do array abaixo
+        [codigo_barras, nome, descricao, quantidade, preco],
+
+        function(err) {
+
+            if (err) {//se der erro mostra mensagem
+                return res.status(500).json({
+                    erro: err.message
+                });
+            }
+            //se receber json do banco, mostra mensagem de sucesso e o id do produto cadastrado
+            res.json({
+                mensagem: "Produto cadastrado!",
+                id: this.lastID
+            });
+        }
+    );
+});
+
+//liga p servidor na porta 3000
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
 });
