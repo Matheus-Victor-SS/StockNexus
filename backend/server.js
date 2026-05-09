01d561d3aeb71bdca8bf600952a6bd76574c1b76
Matheus <matheus.victor.ss48@gmail.com>
2026-05-09 10:36:30 -0300

    atualizando dependencias e adicionando os requisitos e dependencias do server.js

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+const express = require("express");//recebe json
+const cors = require("cors");//permite conversar com front
+const db = require("./database");
+const app = express();
+app.use(cors());
+app.use(express.json());
